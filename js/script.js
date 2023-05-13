c4e30cd94862af80492d0c92a67bbe21a070bfc1
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-13 03:22:32 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Bonnie ZHu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie zhu
+// Created on:SMay 
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function does multiplication.
+ */
+function myButtonClicked() {
+  var numA = parseFloat(document.getElementById("firstInputedNumber").value)
+  var product = 0
+  var counter = 0
+
+  while (counter <= numA) {
+    product = product + counter
+    counter++
+  }
+
+  document.getElementById("product").innerHTML = "The product is: " + product
+}
